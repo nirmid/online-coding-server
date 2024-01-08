a9941919958c6b9a5527819946708bc44e2b341e
nirmid <93215733+nirmid@users.noreply.github.com>
2024-01-08 14:32:46 +0200

    using broadcast in Server

--- server.js
+++ server.js
@@ -44,7 +44,8 @@ io.on('connection', (socket) => {
       ]);
 
       // Emit  update to all clients of the same title
-      socket.to(title).except(socket.id).emit('codeUpdated', { title, updatedCode });
+      //socket.to(title).emit('codeUpdated', { title, updatedCode });
+      socket.broadcast.emit('codeUpdated', { title, updatedCode });
     } catch (error) {
       console.error('Error updating code:', error);
     }
