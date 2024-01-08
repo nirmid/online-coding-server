649b5ff2b97cfe7f5eb20850f8f8d4eaf6040c51
nirmid <93215733+nirmid@users.noreply.github.com>
2024-01-08 12:01:54 +0200

    websocket origin fix

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: 'localhost:3000',
+    origin: '*',
     methods: ['GET', 'POST'],
   },
 });
